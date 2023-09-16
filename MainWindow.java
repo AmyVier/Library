969f924b774aed2c5c662f2ec8c9b5504e7a9615
Amy Vier <amyvier@icloud.com>
2023-09-16 16:08:16 -0700

    git commit MainWindow.java

--- MainWindow.java
+++ MainWindow.java
@@ -4,16 +4,22 @@ import java.awt.event.*;
 public class MainWindow {
     public static void create() {
         JFrame window = new JFrame ("Menu");
-        window.setSize(690,800);
-        window.setVisible(true);
+        window.setLayout(null);
 
         JButton logIn = new JButton("LogIn");
         window.add(logIn);
+        logIn.setBounds(100,100,100,100);
+        JTextField password = new JTextField(16);  
+        password.setBounds(20,10, 100,100);  
+        window.add(password);
 
-        window.addWindowListener(new WindowAdapter(){
-            public void windowClosing(WindowEvent windowEvent){
-              System.exit(0);
-            }
-          });
+        window.setSize(690,800);
+        window.setVisible(true);
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        logIn.addActionListener((ActionEvent e) -> {
+          System.out.println(password.getText());
+        });
     }
 }
---
+++ PasswordManager.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class PasswordManager {
+    HashMap<String, String> username_password= new HashMap<>();
+
+    public boolean findUser(String username, String password) {
+        if (username_password.containsKey(username) && username_password.get(username).equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addUser(String username, String password) {
+        username_password.put(username, password);
+    }
+}
